732a2434c768747e1385a7c2bdbaabd9a69c9b9e
Dominion Gbadamosi <worshipgbadamosi@live.com>
2025-12-22 05:33:52 +0000

    add: line numbers to comment reader

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use regex::Regex;
 use std::fs;
 
 struct Comment {
+    line: usize,
     text: String,
 }
 
@@ -9,7 +10,19 @@ fn main() {
     let contents = fs::read_to_string("example.java").unwrap();
     let re = Regex::new(r"//.*|/\*[\s\S]*?\*/").unwrap();
 
+    let mut comments = Vec::new();
+
     for m in re.find_iter(&contents) {
-        print!("---{}", m.as_str());
+        let start = m.start();
+        let line = contents[..start].matches("\n").count() + 1;
+        comments.push(Comment {
+            text: m.as_str().to_string(),
+            line: line
+        });
+    }
+
+    println!("{} comments found", comments.len());
+    for comment in comments {
+        print!("Line {}: {}", comment.line, comment.text);
     }
 }
